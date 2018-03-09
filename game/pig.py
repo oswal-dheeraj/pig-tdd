e6d2d1966032a5d4c8eebf0b9395a5d75d5c59e2
Dheeraj Oswal <oswal-dheeraj@users.noreply.github.com>
2018-03-09 21:55:37 +0530

    #006: Keep it rollin...

--- game/pig.py
+++ game/pig.py
@@ -5,3 +5,7 @@ class Pig:
     def get_players(self):
         """Return a tuple of all the players"""
         return self.players
+
+    def roll(self):
+        """Return a number between 1 and 6"""
+        pass
