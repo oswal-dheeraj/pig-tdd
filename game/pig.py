3af69523379e83ff3dcb12da71bf7e9301de9413
Dheeraj Oswal <oswal-dheeraj@users.noreply.github.com>
2018-03-09 22:06:36 +0530

    #010: Keeping scores one more time

--- game/pig.py
+++ game/pig.py
@@ -3,9 +3,7 @@ import random
 class Pig:
     def __init__(self, *players):
         self.players = players
-        self.scores = {}
-        for player in self.players:
-            self.scores[player] = 0
+        self.scores = dict.fromkeys(self.players, 0)
 
     def get_players(self):
         """Return a tuple of all the players"""
