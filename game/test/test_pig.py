e7a131312adef09079d3c5a329a33ca269dd1512
Dheeraj Oswal <oswal-dheeraj@users.noreply.github.com>
2018-03-09 21:25:51 +0530

    #002: Our first test case

--- game/test/test_pig.py
+++ game/test/test_pig.py
@@ -1,9 +1,13 @@
 import unittest
 
+import game.pig
+
 
 class TestPig(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_join(self):
+        """Players may join a game of Pig"""
+        pig = game.pig.Pig('PlayerA', 'PlayerB', 'PlayerC')
+        self.assertEqual(pig.get_players(), ('PlayerA', 'PlayerB', 'PlayerC'))
 
 
 if __name__ == '__main__':
